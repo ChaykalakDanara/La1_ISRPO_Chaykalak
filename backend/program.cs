1b04e2e70dd10dffb76f535c8be19eba3972cbf2
ChaykalakDanara <chaykalak.d08@gmail.com>
2026-02-03 16:25:38 +0300

    Add user input for name

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Меня зовут Данара");
-Console.WriteLine("16.01.26 11:39");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Меня зовут Данара");
+// Console.WriteLine("16.01.26 11:39");
+
+using System;
+
+namespace backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine($"\nПривет, {name}!");
+        }
+    }
+}
